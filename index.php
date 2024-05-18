797f69e4d78a0611796f5aaf5e46254739ba7d7f
Rafael Nacle <54647722+rafaelnacle@users.noreply.github.com>
2024-05-18 19:48:33 -0300

    refactor: move DB logic to it's own file

--- index.php
+++ index.php
@@ -1,19 +1,12 @@
 <?php
 
 require 'functions.php';
-// require 'router.php';
+require 'router.php';
+require 'Database.php';
 
-// connect to MySQL database.
+$db = new Database();
 
-//data source name
-$dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
-
-$pdo = new PDO($dsn);
-
-$statement = $pdo->prepare("select * from posts");
-$statement->execute();
-
-$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+$posts = $db->query("select * from posts")->fetchAll(PDO::FETCH_ASSOC);
 
 foreach ($posts as $post) {
   echo "<li>" . $post['title'] . "</li>";
