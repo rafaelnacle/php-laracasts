90a4013a36493e712deadd60dd6c99cb4de2245d
Rafael Nacle <54647722+rafaelnacle@users.noreply.github.com>
2024-05-18 02:15:41 -0300

    refactor: check button highlighting through url

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -8,13 +8,14 @@
         </div>
         <div class="hidden md:block">
           <div class="ml-10 flex items-baseline space-x-4">
-            <a href="/" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium"
+            <a href="/"
+              class="<?= urlIs('/') ? 'bg-gray-900 text-white' : 'text-gray-300' ?> hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
               aria-current="page">Home</a>
             <a href="/about.php"
-              class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">About
+              class="<?= urlIs('/about.php') ? 'bg-gray-900 text-white' : 'text-gray-300' ?> hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">About
               Us</a>
             <a href="/contact.php"
-              class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Contact</a>
+              class="<?= urlIs('/contact.php') ? 'bg-gray-900 text-white' : 'text-gray-300'; ?> hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Contact</a>
           </div>
         </div>
       </div>
