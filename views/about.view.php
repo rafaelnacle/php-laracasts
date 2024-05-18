9d820d148bc59012551464ea6c0f7cd731f99978
Rafael Nacle <54647722+rafaelnacle@users.noreply.github.com>
2024-05-18 01:29:34 -0300

    refactor: separate content into partials for code readability

--- views/about.view.php
+++ views/about.view.php
@@ -1,31 +1,15 @@
-<!DOCTYPE html>
-<html lang="en" class="h-full bg-gray-100">
+<?php require ("partials/head.php") ?>
+<?php require ("partials/nav.php") ?>
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>PHP Website View</title>
-  <script src="https://cdn.tailwindcss.com"></script>
-</head>
-
-<body class="h-full">
-  <div class="min-h-full">
-
-    <?php require ("partials/nav.php") ?>
-
-    <header class="bg-white shadow">
-      <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <h1 class="text-3xl font-bold tracking-tight text-gray-900">About Us</h1>
-      </div>
-    </header>
-    <main>
-      <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-        <p>Welcome to the about page</p>
-      </div>
-    </main>
+<header class="bg-white shadow">
+  <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+    <h1 class="text-3xl font-bold tracking-tight text-gray-900">About Us</h1>
   </div>
+</header>
+<main>
+  <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+    <p>Hello. Welcome to the about us page</p>
+  </div>
+</main>
 
-
-</body>
-
-</html>
+<?php require ("partials/footer.php") ?>
