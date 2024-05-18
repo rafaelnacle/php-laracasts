bb7fde725fd24055c9680876ca5a42121421de66
Rafael Nacle <54647722+rafaelnacle@users.noreply.github.com>
2024-05-18 01:15:23 -0300

    chore: removed unused link

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -136,7 +136,7 @@
           Us</a>
         <a href="/contact.php"
           class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Contact</a>
-        <a href="/mission.php"
+        <a href="/contact.php"
           class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Our
           Mission</a>
       </div>
