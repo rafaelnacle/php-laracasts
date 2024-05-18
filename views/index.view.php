adad352e094fa1c4f2c18d29dbefdf2f5961a2be
Rafael Nacle <54647722+rafaelnacle@users.noreply.github.com>
2024-05-18 01:31:49 -0300

    refactor: remove banner to be a partial file

--- views/index.view.php
+++ views/index.view.php
@@ -1,11 +1,8 @@
 <?php require ("partials/head.php") ?>
 <?php require ("partials/nav.php") ?>
 
-<header class="bg-white shadow">
-  <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-    <h1 class="text-3xl font-bold tracking-tight text-gray-900">Home</h1>
-  </div>
-</header>
+<?php require ("partials/banner.php") ?>
+
 <main>
   <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
     <p>Hello. Welcome to the home page</p>
