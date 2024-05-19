bbe089d801b3c3bec566fc2d1a342fb809de61ac
Rafael Nacle <54647722+rafaelnacle@users.noreply.github.com>
2024-05-19 16:31:09 -0300

    refactor: use new view and base_path functions for better reusability

--- controllers/notes/create.php
+++ controllers/notes/create.php
@@ -1,14 +1,13 @@
 <?php
 
-require 'Validator.php';
+require base_path('Core/Validator.php');
 
-$config = require 'config.php';
+$config = require base_path('config.php');
 $db = new Database($config['database']);
 
-$heading = 'Create Note';
+$errors = [];
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  $errors = [];
   if (!Validator::string($_POST['body'], 1, 1000)) {
     $errors['body'] = 'A body of no more than 1,000 characters is required';
   }
@@ -21,4 +20,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   }
 }
 
-require 'views/notes/create.view.php';
+view("notes/create.view.php", [
+  'heading' => 'Create Note',
+  'errors' => $errors
+]);
