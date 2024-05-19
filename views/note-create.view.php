5c9d9b10fcd431422e1a28ef65824064e599873b
Rafael Nacle <54647722+rafaelnacle@users.noreply.github.com>
2024-05-19 15:21:16 -0300

    fix: form action and spaces on the textarea

--- views/note-create.view.php
+++ views/note-create.view.php
@@ -4,21 +4,19 @@
 
 <main>
   <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-    <form action="#" method="POST">
+    <form method="POST">
       <div class="space-y-12">
         <div class="border-b border-gray-900/10 pb-12">
           <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
             <div class="col-span-full">
-              <label for="about" class="block text-sm font-medium leading-6 text-gray-900">Body</label>
+              <label for="body" class="block text-sm font-medium leading-6 text-gray-900">Body</label>
               <div class="mt-2">
                 <textarea id="body" name="body" rows="3"
                   class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  placeholder="Here's an idea for a note...">
-                 <?= $_POST['body'] ?? '' ?> 
-                </textarea>
+                  placeholder="Here's an idea for a note..."><?= isset($_POST['body']) ? $_POST['body'] : '' ?></textarea>
 
                 <?php if (isset($errors['body'])): ?>
-                  <p class="text-red-500 text-xs mt-2"><?= $errors['body'] ?></p>
+                  <p class="text-red-500 text-xs mt-2"><?= htmlspecialchars($errors['body']) ?></p>
                 <?php endif; ?>
               </div>
             </div>
