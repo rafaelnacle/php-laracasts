aba97205deb684e64fdcb8522a03cfec6921b13b
Rafael Nacle <54647722+rafaelnacle@users.noreply.github.com>
2024-05-19 17:21:15 -0300

    feat: delete a note

--- controllers/notes/show.php
+++ controllers/notes/show.php
@@ -7,13 +7,28 @@ $db = new Database($config['database']);
 
 $currentUserId = 1;
 
-$note = $db->query('select * from notes where id = :id', [
-  'id' => $_GET['id']
-])->findOrFail();
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $note = $db->query('select * from notes where id = :id', [
+    'id' => $_GET['id']
+  ])->findOrFail();
 
-authorize($note['user_id'] === $currentUserId);
+  authorize($note['user_id'] === $currentUserId);
 
-view("notes/show.view.php", [
-  'heading' => 'Note',
-  'note' => $note
-]);
+  $db->query('delete from notes where id = :id', [
+    'id' => $_GET['id'],
+  ]);
+
+  header('location: /notes');
+  exit();
+} else {
+  $note = $db->query('select * from notes where id = :id', [
+    'id' => $_GET['id']
+  ])->findOrFail();
+
+  authorize($note['user_id'] === $currentUserId);
+
+  view("notes/show.view.php", [
+    'heading' => 'Note',
+    'note' => $note
+  ]);
+}
