c8b87d6082bd788e37f4b1fe2bdd63b6c58f6fa4
Rafael Nacle <54647722+rafaelnacle@users.noreply.github.com>
2024-05-19 00:02:47 -0300

    feat: add link for notes

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -14,6 +14,8 @@
             <a href="/about"
               class="<?= urlIs('/about') ? 'bg-gray-900 text-white' : 'text-gray-300' ?> hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">About
               Us</a>
+            <a href="/notes"
+              class="<?= urlIs('/about') ? 'bg-gray-900 text-white' : 'text-gray-300' ?> hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Notes</a>
             <a href="/contact"
               class="<?= urlIs('/contact') ? 'bg-gray-900 text-white' : 'text-gray-300'; ?> hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Contact</a>
           </div>
